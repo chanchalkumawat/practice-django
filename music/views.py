95e327661a96710adeae95b92fd0ae10956d7f4e
shinigangrade <shinigangrade@gmail.com>
2018-04-27 11:29:21 +0530

    changes by user2

--- music/views.py
+++ music/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>This is the Music app homepage in branch1.</h1>")
+    return HttpResponse("<h1>This is the Music app homepage in branch1 ----- abcd.</h1>")
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for album ID : %s.</h2>" % album_id)
